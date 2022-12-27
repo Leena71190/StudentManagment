8e497067ca846499dff5cb305cfb4fe8776a5f2a
leena71190 <leena71190@gmail.com>
2022-12-27 22:42:20 +0530

    add new file

---
+++ add.js
@@ -0,0 +1,36 @@
+
+Neha = 382
+Sweta = 284
+Kabir = 543
+Sonia = 496
+Ritu = 200
+Radha = 473
+Preeti = 327
+totalmarks= 600
+console.log("students= Neha, Sweta, Kabir, Sonia, Ritu, Radha, Preeti")
+total_percentage_Neha = Math.round((382/600)*100) 
+console.log("total_percentage " + " Neha " + " % " + total_percentage_Neha)
+total_percentage_Sweta = Math.round((284/600)*100)
+console.log("total_percentage " + " Sweta " + " % " + total_percentage_Sweta)
+total_percentage_Kabir = Math.round((543/600)*100)
+console.log("total_percentage " + " Kabir " + " % " + total_percentage_Kabir)
+total_percentage_Sonia = Math.round((496/600)*100)
+console.log("total_percentage " + " Sonia " + " % " + total_percentage_Sonia)
+total_percentage_Ritu = Math.round((200/600)*100)
+console.log("total_percentage " + " Ritu " + " % " + total_percentage_Ritu)
+total_percentage_Radha = Math.round((473/600)*100)
+console.log("total_percentage " + " Radha " + " % " + total_percentage_Radha)
+total_percentage_Preeti = Math.round((327/600)*100)
+console.log("total_percentage " + " Preeti " + " % " + total_percentage_Preeti)
+if (total_percentage_Neha < 33)
+console.log("Neha Failed")
+if(total_percentage_Neha > 33 && total_percentage_Neha < 50)
+console.log ("Neha got Third Division")
+if(total_percentage_Neha > 50 && total_percentage_Neha < 60)
+console.log ("Neha got Second Division")
+if(total_percentage_Neha > 60 && total_percentage_Neha < 70)
+console.log ("Neha got First Division")
+if(total_percentage_Neha > 70 && total_percentage_Neha < 100)
+console.log ("Neha got top of the School")
+
+
